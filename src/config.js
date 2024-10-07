372bc6165124d5634249ae86610284c27657c54d
Alexander Dung Dung <65543943+Alexander8437@users.noreply.github.com>
2024-10-07 17:32:29 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
   // baseUrl: "http://147.79.67.201:8081/MS/Motherson/crm/v1",
   // baseUrl: "https://c878-14-96-97-220.ngrok-free.app/Motherson/crm/v1",
-  baseUrl: "http://localhost:8080/Motherson/crm/v1",
+  baseUrl: "http://147.79.67.201:8081/MS/Motherson/crm/v1",
 };
 
 export default config;
